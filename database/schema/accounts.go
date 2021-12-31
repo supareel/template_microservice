ae07a1581b4a7fa0c87aa61ee0f28b55c7474b0a
sourabhmandal <19mandal97@gmail.com>
2021-12-31 17:07:45 +0530

    remove uneccesary ent files

--- database/schema/accounts.go
+++ database/schema/accounts.go
@@ -16,8 +16,8 @@ type Accounts struct {
 func (Accounts) Fields() []ent.Field {
 	return []ent.Field{
 		field.String("owner").NotEmpty(),
-		field.Int64("balance").Positive(),
-		field.String("currency").NotEmpty(),
+		field.Int64("balance").Positive().Default(0),
+		field.String("currency").NotEmpty().Default("INR"),
 		field.Time("created_at").Default(time.Now().UTC),
 	}
 }
