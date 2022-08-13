35e64bb6e8eb0ff9fc5e388769ceb67e989a46d3
sourabhmandal <19mandal97@gmail.com>
2022-08-14 01:17:54 +0530

    add husky

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -13,20 +13,20 @@ import (
 )
 
 func main() {
-    connectTo := "127.0.0.1:7005"
-    conn, err := grpc.Dial(connectTo, grpc.WithBlock(), grpc.WithInsecure())
-    if err != nil {
-          fmt.Errorf("failed to connect to PetStoreService on %s: %w", connectTo, err)
-      return
-    }
-    log.Println("Connected to", connectTo)
+	connectTo := "127.0.0.1:7005"
+	conn, err := grpc.Dial(connectTo, grpc.WithBlock(), grpc.WithInsecure())
+	if err != nil {
+		  fmt.Errorf("failed to connect to PetStoreService on %s: %w", connectTo, err)
+		return
+	}
+	log.Println("Connected to", connectTo)
 
-    taskService := proto.NewTaskServiceClient(conn)
-    resp, err := taskService.GetAllTasks(context.Background(), &proto.GetAllTasksRequest{})
-    if err != nil {
-     fmt.Errorf("failed to PutPet: %w", err)
-     return
-    }
+	taskService := proto.NewTaskServiceClient(conn)
+	resp, err := taskService.GetAllTasks(context.Background(), &proto.GetAllTasksRequest{})
+	if err != nil {
+		fmt.Errorf("failed to PutPet: %w", err)
+		return
+	}
 
-    fmt.Printf("DATA ::\n%+v\nSTATUS ::\n%+v", resp.Data, resp.Status)
-  }
+	fmt.Printf("DATA ::\n%+v\nSTATUS ::\n%+v", resp.Data, resp.Status)
+}
