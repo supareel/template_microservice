8686507d4204a2998d77c01ae3079b5759e67cb6
sourabhmandal <19mandal97@gmail.com>
2024-02-20 00:54:04 +0530

    ci: create ci files

--- src/internal/taskmanager/taskmanager_test.go
+++ src/internal/taskmanager/taskmanager_test.go
@@ -1,38 +1,31 @@
 package taskmanager_test
 
 import (
-	"context"
-	"path/filepath"
 	"testing"
-	"time"
-
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/postgres"
-	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestTaskManagerRepository(t *testing.T) {
-	ctx := context.Background()
+	// ctx := context.Background()
 
-	pgContainer, err := postgres.RunContainer(ctx,
-		testcontainers.WithImage("postgres:15.3-alpine"),
-		postgres.WithInitScripts(filepath.Join("..", "testdata", "init-db.sql")),
-		postgres.WithDatabase("test-db"),
-		postgres.WithUsername("postgres"),
-		postgres.WithPassword("postgres"),
-		testcontainers.WithWaitStrategy(
-			wait.ForLog("database system is ready to accept connections").
-				WithOccurrence(2).WithStartupTimeout(5*time.Second)),
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
+	// pgContainer, err := postgres.RunContainer(ctx,
+	// 	testcontainers.WithImage("postgres:15.3-alpine"),
+	// 	postgres.WithInitScripts(filepath.Join("..", "testdata", "init-db.sql")),
+	// 	postgres.WithDatabase("test-db"),
+	// 	postgres.WithUsername("postgres"),
+	// 	postgres.WithPassword("postgres"),
+	// 	testcontainers.WithWaitStrategy(
+	// 		wait.ForLog("database system is ready to accept connections").
+	// 			WithOccurrence(2).WithStartupTimeout(5*time.Second)),
+	// )
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
 
-	t.Cleanup(func() {
-		if err := pgContainer.Terminate(ctx); err != nil {
-			t.Fatalf("failed to terminate pgContainer: %s", err)
-		}
-	})
+	// t.Cleanup(func() {
+	// 	if err := pgContainer.Terminate(ctx); err != nil {
+	// 		t.Fatalf("failed to terminate pgContainer: %s", err)
+	// 	}
+	// })
 
 	// connStr, err := pgContainer.ConnectionString(ctx, "sslmode=disable")
 	// assert.NoError(t, err)
